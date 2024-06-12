dfa018d5a9aeec8886b4cc693004729053055ffc
uzimaru0000 <shuji365630@gmail.com>
2024-06-12 21:18:11 +0900

    support string-format

--- src/prompt/config.rs
+++ src/prompt/config.rs
@@ -3,7 +3,7 @@ use inquire::ui::{Color, RenderConfig, Styled};
 pub fn render_config() -> RenderConfig {
     RenderConfig {
         prompt_prefix: Styled::new("*").with_fg(Color::LightGreen),
-        password_mask: '#',
+        password_mask: '*',
         ..Default::default()
     }
 }
@@ -12,7 +12,7 @@ pub fn render_config_with_skkipable() -> RenderConfig {
     RenderConfig {
         canceled_prompt_indicator: Styled::new("Skipped").with_fg(Color::LightRed),
         prompt_prefix: Styled::new("?").with_fg(Color::LightGreen),
-        password_mask: '#',
+        password_mask: '*',
         ..Default::default()
     }
 }
